f8cde9f57df8125e6d22ed5bb83c6535bdf450e1
ekhlasmridha <ekhlasmridha@gmail.com>
2022-09-07 02:10:34 +0600

    theme

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,31 @@
+import "./Layout.scss";
+
+export const Layout = (props: any) => {
+  return (
+    <>
+      <header className="header-toolbar">
+        <nav>
+          <ul className="nav-container">
+            <li>
+              <div className="nav-item">About</div>
+            </li>
+            <li>
+              <div className="nav-item">Works</div>
+            </li>
+            <li>
+              <div className="nav-item">Contact</div>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="landing-body">
+        alksd kl hkdjashdkjasdh kljahdklasjhd askljd h alksd kl hkdjashdkjasdh
+        kljahdklasjhd askljd h alksd kl hkdjashdkjasdh kljahdklasjhd askljd h
+        alksd kl hkdjashdkjasdh kljahdklasjhd askljd h alksd kl hkdjashdkjasdh
+        kljahdklasjhd askljd h alksd kl hkdjashdkjasdh kljahdklasjhd askljd h
+        alksd kl hkdjashdkjasdh kljahdklasjhd askljd h alksd kl hkdjashdkjasdh
+        kljahdklasjhd askljd h alksd kl hkdjashdkjasdh kljahdklasjhd askljd h
+      </div>
+    </>
+  );
+};
