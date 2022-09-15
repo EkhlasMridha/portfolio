670f6b41d4c839b37993726853841a9217f30b0c
ekhlasmridha <ekhlasmridha@gmail.com>
2022-09-16 01:29:34 +0600

    landing responsive

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -10,13 +10,12 @@ export const Landing = (props: any) => {
     >
       <div className="landing-content content-container padding-side-20">
         <div className="left-content primary-color" key={"lefcontent"}>
-          <h1>Hi! I am,</h1>
-          <h1 className="myname">Ekhlas Ali Mridha</h1>
-          <div className="highlight-color typewriter-text">
-            {/* <TypeWriter texts={["Web Developer", "UI/UX Designer"]} /> */}
-          </div>
+          <h1 className="uppercase greeting">
+            Hi<span className="highlight-color">!</span> I am,
+          </h1>
+          <h1 className="myname uppercase">Ekhlas Ali Mridha</h1>
         </div>
-        <div className="graphics-container" key={"graphics"}>
+        <div className="graphics-container justify-center" key={"graphics"}>
           <LandingAvatar className="brand-avatar" />
           <SvgLanding />
         </div>
