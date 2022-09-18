782c0fba29185d88a41af8568c03888b6f945ba4
ekhlasmridha <ekhlasmridha@gmail.com>
2022-09-19 01:35:08 +0600

    landing work type display

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,8 +1,37 @@
+import { useEffect, useLayoutEffect, useMemo, useState } from "react";
 import "./Landing.scss";
 import { LandingAvatar, SvgLanding } from "./SvgLanding";
 
 export const Landing = (props: any) => {
-  console.log("Working 1");
+  const allTexts: string[] = ["Web Developer", "UI/UX Designer"];
+  const [index, setIndex] = useState<number>(0);
+  let count = 0;
+  var suffleWorkType = (containerId: string) => {
+    let workType = document.getElementsByClassName(
+      containerId
+    ) as HTMLCollectionOf<HTMLElement>;
+    for (let i = 0; i < workType?.length; ++i) {
+      workType[i].style.display = "none";
+    }
+    workType[index].style.display = "block";
+  };
+
+  useEffect(() => {
+    suffleWorkType("work-type");
+  }, []);
+
+  useEffect(() => {
+    console.log("Aow");
+
+    setTimeout(() => {
+      suffleWorkType("work-type");
+      setIndex((pre) => {
+        let newIndex = pre + 1 > allTexts.length - 1 ? 0 : pre + 1;
+        return newIndex;
+      });
+    }, 3000);
+  }, [index]);
+
   return (
     <section
       key={"landing"}
@@ -14,6 +43,8 @@ export const Landing = (props: any) => {
             Hi<span className="highlight-color">!</span> I am,
           </h1>
           <h1 className="myname uppercase">Ekhlas Ali Mridha</h1>
+          <div className="work-type highlight-lighten">Web Developer</div>
+          <div className="work-type highlight-lighten">UI/UX Designer</div>
         </div>
         <div className="graphics-container justify-center" key={"graphics"}>
           <LandingAvatar className="brand-avatar" />
