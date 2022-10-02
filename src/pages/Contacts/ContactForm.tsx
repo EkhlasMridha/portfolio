a0f6453d953ec705e7483d090153372dc1fe3e11
ekhlasmridha <ekhlasmridha@gmail.com>
2022-10-03 01:00:50 +0600

    notification on message sent

--- src/pages/Contacts/ContactForm.tsx
+++ src/pages/Contacts/ContactForm.tsx
@@ -125,9 +125,6 @@ export const ContactForm = (props: any) => {
           <span>Sending...</span>
         </div>
       </div>
-      <button type="button" onClick={showToaster}>
-        Test 2
-      </button>
     </form>
   );
 };
