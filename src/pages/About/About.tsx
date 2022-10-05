e63005ddac47ef2de6342ef0b949466ac1e5f49e
ekhlasmridha <ekhlasmridha@gmail.com>
2022-10-05 12:34:42 +0600

    content modified

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -8,19 +8,21 @@ export const About = (props: any) => {
       id="about"
     >
       <div className="about-content content-container padding-side-20">
-        <h2 className="cursor-text">ABOUT</h2>
-        <p>
-          I am a web developer skilled with various web technologies. I like to
-          push myself beyond my limit to do something new and creative. I always
-          keep myself up to date with the latest technologies and passionate to
-          learn new things.
-        </p>
-        <p>
-          I don’t like to define myself by the work I have done. I want to
-          define myself by the work I want to do. I like to take challenges
-          whenever it comes to do something out of the box. It’s not only just
-          about my career, it’s my passion to do exciting works.
-        </p>
+        <h2 className="cursor-text about-header">ABOUT</h2>
+        <div className="about-description">
+          <p>
+            I am a web developer skilled with various web technologies. I like
+            to push myself beyond my limit to do something new and creative. I
+            always keep myself up to date with the latest technologies and
+            passionate to learn new things.
+          </p>
+          <p>
+            I don’t like to define myself by the work I have done. I want to
+            define myself by the work I want to do. I like to take challenges
+            whenever it comes to do something out of the box. It’s not only just
+            about my career, it’s my passion to do exciting works.
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Layout } from "../Layout/Layout";
 // import { PageLoader } from "../shared/PageLoader/PageLoader";
 import { About } from "./About/About";
@@ -6,6 +7,38 @@ import { Landing } from "./Landing/Landing";
 import { Works } from "./Works/Works";
 
 const RootPage = () => {
+  useEffect(() => {
+    let content = document.querySelector(".about-container");
+    if ((content ?? "") !== "") {
+      obServeAndAnimateAbout(content as Element);
+    }
+  }, []);
+
+  function obServeAndAnimateAbout(content: Element) {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const aboutDesc = entry.target.querySelector(
+          ".about-description"
+        ) as Element;
+        const aboutHeader = entry.target.querySelector(
+          ".about-header"
+        ) as Element;
+
+        if (entry.isIntersecting) {
+          aboutDesc.classList.add("animate-desc");
+          aboutHeader.classList.add("animate-about-header");
+          return; // if we added the class, exit the function
+        }
+
+        // We're not intersecting, so remove the class!
+        aboutDesc.classList.remove("animate-desc");
+        aboutHeader.classList.remove("animate-about-header");
+      });
+    });
+
+    observer.observe(content);
+  }
+
   return (
     <>
       <Layout>
